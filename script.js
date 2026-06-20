54c9f99bc32e5efb5a24103b74aee1aff3457c3c
Vey Vichhat <veyvichhat18@gmail.com>
2026-06-20 15:47:39 +0700

    Updated web design

---
+++ script.js
@@ -0,0 +1,13 @@
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+// Contact Form
+document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    alert("Thank you! Your message has been sent.");
+});
